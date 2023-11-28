dccaba9d215e9aee7b32ca225868897fd8007c68
morenow <jjang9877@gmail.com>
2023-11-28 21:16:53 +0900

    first commit

---
+++ src/main/java/cotato/cotatomanage/CotatoManageApplication.java
@@ -0,0 +1,13 @@
+package cotato.cotatomanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotatoManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotatoManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cotato/cotatomanage/CotatoManageApplicationTests.java
@@ -0,0 +1,13 @@
+package cotato.cotatomanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotatoManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
