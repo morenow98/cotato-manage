cebcdb671ef15385b805aa7a16f31f2fc174eaf9
morenow <jjang9877@gmail.com>
2023-12-18 03:41:25 +0900

    fix: 기수마다 실력 향상이 1로 고정되는 오류 수정

--- src/main/java/cotato/cotatomanage/domain/Member.java
+++ src/main/java/cotato/cotatomanage/domain/Member.java
@@ -4,11 +4,14 @@ import cotato.cotatomanage.domain.enums.Event;
 import cotato.cotatomanage.domain.enums.Part;
 import java.time.LocalDate;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 public class Member {
 
     private static int memberNumber;
     private static final int INELIGIBLE_MIN_AGE_FOR_EVENT = 27;
+    private static final int ABILITY_INCREASE_PER_PERIOD = 2;
 
     private final int id;
     private String name;
@@ -27,7 +30,7 @@ public class Member {
 
     public int calculateAbility(int currentPeriod, LocalDate now) {
         int ability = this.age;
-        ability += currentPeriod - this.period;
+        ability += (currentPeriod - this.period) * ABILITY_INCREASE_PER_PERIOD;
         if (age < INELIGIBLE_MIN_AGE_FOR_EVENT) {
             ability += Event.getIncreasingAbility(this, now);
         }
